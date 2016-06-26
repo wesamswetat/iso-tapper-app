8e5afd4a39c472b064b695ce4c320156663da602
Wesam <wesam@Wesams-MacBook-Air.local>
2016-06-26 09:21:48 +0300

    simpel IPONE App --Like a game of TAPS

--- tapperApp/ViewController.swift
+++ tapperApp/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CuinButton: UIButton!
+    @IBOutlet weak var PlayButton: UIButton!
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var TapsLabel: UILabel!
+    @IBOutlet weak var TapsText: UITextField!
+    
+    var howManyToTap: Int = 0
+    var tapping: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func CuinButton(sender: AnyObject) {
+        
+        tapping += 1
+        TapsLabel.text = "\(tapping) Taps"
+        
+        if tapping == howManyToTap {
+            
+            Logo.hidden = false
+            TapsText.hidden = false
+            PlayButton.hidden = false
+            CuinButton.hidden = true
+            TapsLabel.hidden = true
+            howManyToTap = 0
+            tapping = 0
+        }
+        
+    }
 
 
+    @IBAction func PlayButton(sender: AnyObject) {
+        
+        Logo.hidden = true
+        TapsText.hidden = true
+        howManyToTap = Int(TapsText.text!)!
+        TapsText.text = ""
+        PlayButton.hidden = true
+        CuinButton.hidden = false
+        TapsLabel.hidden = false
+        
+    }
 }
 
